5a9211546a7e992e3527242f3e1c425f70ea07b8
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-03-06 21:46:25 +0100

    Cleanup

--- Source/MounteaExamples/MounteaExamples.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MounteaExamples.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MounteaExamples, "MounteaExamples" );
