b97eaeff04434a89c1c5480960735f81b5e9247e
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-04-27 20:39:07 +0200

    Mountea Dialogue System update

--- Plugins/MounteaDialogueSystem/Source/MounteaDialogueSystem/Private/Data/DialogueAdditionalData.cpp
+++
@@ -1,4 +0,0 @@
-// All rights reserved Dominik Pavlicek 2023
-
-
-#include "Data/DialogueAdditionalData.h"
--- Plugins/MounteaDialogueSystem/Source/MounteaDialogueSystem/Public/Helpers/MounteaDialogueGraphHelpers.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Dominik Pavlicek 2022. All Rights Reserved.
-
-#include "Helpers/MounteaDialogueGraphHelpers.h"
-
-DEFINE_LOG_CATEGORY(LogMounteaDialogueSystem);
