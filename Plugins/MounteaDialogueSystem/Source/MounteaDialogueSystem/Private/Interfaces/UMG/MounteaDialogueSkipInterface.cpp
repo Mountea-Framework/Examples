3ca465ddb3de188ed941e53a6b81aa5804a66a42
Dominik Morse <dominikpavlicek.business@gmail.com>
2024-10-24 16:17:58 +0200

    Cleanup 5.4

--- Plugins/MounteaDialogueSystem/Source/MounteaDialogueSystem/Private/Interfaces/UMG/MounteaDialogueSkipInterface.cpp
+++
@@ -1,7 +0,0 @@
-﻿// All rights reserved Dominik Morse (Pavlicek) 2024.
-
-
-#include "Interfaces/UMG/MounteaDialogueSkipInterface.h"
-
-
-// Add default functionality here for any IMounteaDialogueSkipInterface functions that are not pure virtual.
